bf2131ac241e49a7df56e3a6b1d75c889ffac048
John Schwenck <jschwenck@stat.tamu.edu>
2020-12-05 01:40:47 -0600

    re-added data files and corresponding documentation to avoid R > 3.5 error

--- R/data.R
+++ R/data.R
@@ -1,13 +1,16 @@
 #' Blood Pressure - John Schwenck
 #'
-#' @source <google.com>
-#' @export
+#' @source <https://doi.org/10.7910/DVN/EA1SAP>
 #'
 "bp_jhs"
 
 
 #' HYPNOS Study - 5 Subject Sample
 #'
-#' @export
+#' Dexcom G4 CGM measurements for 5 subjects with Type II diabetes. These data are part of a larger study sample that consisted of patients with Type 2 diabetes recruited from the general community. To be eligible, patients with Type 2 diabetes, not using insulin therapy and with a glycosylated hemoglobin (HbA$_1c$) value at least 6.5
+#' ABPM measurements
+#'
+#' description of variables
+#'
 #'
 "hypnos_data"
