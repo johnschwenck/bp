f6ddaf0026a29b1a309f708063cc5f4cf746e374
John Schwenck <jschwenck@stat.tamu.edu>
2020-12-05 01:25:39 -0600

    restructured process_data function to avoid build errors

--- R/data_process.R
+++ R/data_process.R
@@ -1,29 +1,38 @@
 
 #' Data Pre-Processor
 #'
-#' @description A helper function to assist in pre-processing the input data for use with other functions.
-#' Typically, this function will process the data and be stored as another dataframe. See Vignette for further details
+#' @description A helper function to assist in pre-processing the user-supplied
+#' input data for use with other functions.
+#' Typically, this function will process the data and be stored as another
+#' dataframe. See Vignette for further details.
 #'
-#' @param data User-supplied dataset containing blood pressure data. Must contain data for Systolic blood pressure and Diastolic blood pressure at a minimum.
-#' @param sbp Required column name string corresponding to Systolic Blood Pressure (mmHg)
-#' @param dbp Required column name string corresponding to Diastolic Blood Pressure (mmHg)
-#' @param bp_datetime Optional column name string corresponding to Date/Time, but HIGHLY recommended to supply if available.
-#' @param id Optional column name string corresponding to subject ID. Typically needed for data corresponding to more than one subject.
-#' @param wake Optional column name string corresponding to sleep status. A WAKE value of 1 indicates that the subject is awake and 0 implies asleep.
+#' @param data User-supplied dataset containing blood pressure data. Must
+#' contain data for Systolic blood pressure and Diastolic blood pressure at a
+#' minimum.
+#' @param sbp Required column name string corresponding to Systolic Blood
+#' Pressure (mmHg)
+#' @param dbp Required column name string corresponding to Diastolic Blood
+#' Pressure (mmHg)
+#' @param bp_datetime Optional column name string corresponding to Date/Time,
+#' but HIGHLY recommended to supply if available.
+#' @param id Optional column name string corresponding to subject ID. Typically
+#' needed for data corresponding to more than one subject.
+#' @param wake Optional column name string corresponding to sleep status. A
+#' WAKE value of 1 indicates that the subject is awake and 0 implies asleep.
 #' @param visit Optional column name string corresponding to Visit number
 #' @param hr Optional column name string corresponding to Heart Rate (bpm)
-#' @param pp Optional column name string corresponding to Pulse Pressure (SBP - DBP). If not supplied, it will be calculated automatically.
-#' @param map Optional column name string corresponding to Mean Arterial Pressure
-#' @param rpp Optional column name string corresponding to Rate Pulse Pressure (SBP * HR). If not supplied, but HR column available, will be calculated automatically.
+#' @param pp Optional column name string corresponding to Pulse Pressure
+#' (SBP - DBP). If not supplied, it will be calculated automatically.
+#' @param map Optional column name string corresponding to Mean Arterial
+#' Pressure
+#' @param rpp Optional column name string corresponding to Rate Pulse
+#' Pressure (SBP * HR). If not supplied, but HR column available, then
+#' RPP will be calculated automatically.
 #'
 #' @return A processed dataframe object
 #' @export
 #'
 #' @examples
-#' load('data/hypnos_data_sample.rda')
-#' data <- process_data(data = hypnos_data, bp_datetime = DATE.TIME, id = ID, wake = WAKE, visit = VISIT, sbp = SYST, dbp = DIAST, hr = HR, map = MAP)
-#' # Note that in the above example, the rpp and pp arguments were intentionally left out (left as NULL), and the process_data function was able to detect it.
-#' # If there was no rpp or pp columns provided, they will be automatically calculated
 process_data <- function(data, sbp = NULL, dbp = NULL, bp_datetime = NULL, id = NULL, wake = NULL, visit = NULL, hr = NULL, pp = NULL, map = NULL, rpp = NULL){
 
 
@@ -34,14 +43,14 @@ process_data <- function(data, sbp = NULL, dbp = NULL, bp_datetime = NULL, id = 
 
       stop('Invalid data type. Please use either data.frame or matrix')
 
-      }else{
+    }else{
 
-        warning('Converted matrix data type to data.frame')
+      warning('Converted matrix data type to data.frame')
 
-        data <- as.data.frame(data)
+      data <- as.data.frame(data)
 
-        }
     }
+  }
 
 
   # Convert all column names to upper case for consistency
@@ -54,9 +63,9 @@ process_data <- function(data, sbp = NULL, dbp = NULL, bp_datetime = NULL, id = 
   # Throw error if SBP and DBP columns aren't specified
   if(is.null(sbp) | is.null(dbp)){
 
-      stop('Both "SBP" and "DBP" column names must be specified.')
+    stop('Both "SBP" and "DBP" column names must be specified.')
 
-    }
+  }
 
 
 
@@ -66,7 +75,7 @@ process_data <- function(data, sbp = NULL, dbp = NULL, bp_datetime = NULL, id = 
 
     if(toupper(sbp) %in% colnames(data) == FALSE){
 
-      warning('Could not find user-defined SBP argument name in dataset. \ni.e. for example, if user improperly defines sbp = "syst" but there is no column name in the dataset, then there will be no matches for "syst". Check spelling of SBP argument.')
+      warning('Could not find user-defined SBP argument name in dataset. \ni.e. for example, if user improperly defines sbp = "syst" but that column name does not exist in the dataset, then there will be no matches for "syst". Check spelling of SBP argument.')
 
       if(length(grep(paste("\\bSBP\\b", sep = ""), names(data))) == 1){
 
@@ -88,31 +97,7 @@ process_data <- function(data, sbp = NULL, dbp = NULL, bp_datetime = NULL, id = 
   } else {
     stop('User-defined SBP name must be character.')
   }# if working with numeric below, remove this bracket
-  {
-  # }else{
-  #
-  #   if(sbp > ncol(data)){
-  #
-  #     stop('Invalid index for SBP. Index greater than number of available columns')
-  #   }
-  #
-  #   if(sbp < 0){
-  #
-  #     stop('Invalid index for SBP. Cannot have negative index')
-  #   }
-  #
-  #   if( (ncol(data) == 2) & (sbp == 2) ){
-  #
-  #     data <- data[ , c(2 , 1)]
-  #
-  #   }else if(sbp != 1){
-  #
-  #     data <- data[, c(sbp, (1:ncol(data))[-sbp])]
-  #     #data[ , sbp] <- as.numeric(data[ , sbp])
-  #
-  #   }
-  # }
-}
+
 
 
 
@@ -142,33 +127,7 @@ process_data <- function(data, sbp = NULL, dbp = NULL, bp_datetime = NULL, id = 
     }
   } else {
     stop('User-defined DBP name must be character.')
-    }# if working with numeric below, remove this bracket
-  {
-  # }else{
-  #
-  #   if(dbp > ncol(data)){
-  #
-  #     stop('Invalid index for DBP. Index greater than number of available columns')
-  #   }
-  #
-  #   if(dbp < 0){
-  #
-  #     stop('Invalid index for DBP. Cannot have negative index')
-  #   }
-  #
-  #   if(sbp == 1){
-  #
-  #     data <- data[, c(1, dbp, (2:ncol(data))[-dbp+1])]
-  #
-  #   }else if(dbp != 2){
-  #
-  #     dbp <- dbp + 1
-  #     data <- data[, c(1, dbp, (2:ncol(data))[-dbp+1])]
-  #     #data[ , 2] <- as.numeric(data[ , 2])
-  #
-  #     }
-  #   }
-}
+  }
 
 
 
@@ -179,6 +138,7 @@ process_data <- function(data, sbp = NULL, dbp = NULL, bp_datetime = NULL, id = 
     if(length(grep(paste("\\bPP\\b", sep = ""), names(data))) == 0){
 
       data$PP <- data$SBP - data$DBP
+      warning('No PP column found. Automatically generated from SBP and DBP columns.')
 
     }
 
@@ -201,32 +161,13 @@ process_data <- function(data, sbp = NULL, dbp = NULL, bp_datetime = NULL, id = 
       data <- data[, c(1:2, col_idx, (3:ncol(data))[-col_idx+2])]
 
       data$PP <- as.numeric(data$PP)
-      }
+    }
   } else {
 
-      stop('User-defined PP name must be character.')
+    stop('User-defined PP name must be character.')
   }
 
-  {
-  # }else{ # if numeric
-  #
-  #   if(pp > ncol(data)){
-  #
-  #     stop('Invalid index for PP. Index greater than number of available columns')
-  #   }
-  #
-  #   if(pp < 0){
-  #
-  #     stop('Invalid index for PP. Cannot have negative index')
-  #   }
-  #
-  #   data <- data[, c(1:2, pp, (3:ncol(data))[-pp + 2])]
-  #
-  #   idx_list$hr + 1
-  #   idx_list$map + 1
-  #   idx_list$rpp + 1
-  # }
-}
+
 
 
 
@@ -242,51 +183,22 @@ process_data <- function(data, sbp = NULL, dbp = NULL, bp_datetime = NULL, id = 
 
   } else if(is.character(hr)){
 
-      if(toupper(hr) %in% colnames(data) == FALSE){
+    if(toupper(hr) %in% colnames(data) == FALSE){
 
-        stop('User-defined HR name does not match column name of supplied dataset')
+      stop('User-defined HR name does not match column name of supplied dataset')
 
-      }else{
+    }else{
 
-        col_idx <- grep(paste("\\b",toupper(hr),"\\b", sep = ""), names(data))
-        colnames(data)[col_idx] <- "HR"
-        data <- data[, c(1:3, col_idx, (4:ncol(data))[-col_idx+3])]
-        data$HR <- as.numeric(data$HR)
-      }
-    } else {
-      stop('User-defined HR name must be character.')
+      col_idx <- grep(paste("\\b",toupper(hr),"\\b", sep = ""), names(data))
+      colnames(data)[col_idx] <- "HR"
+      data <- data[, c(1:3, col_idx, (4:ncol(data))[-col_idx+3])]
+      data$HR <- as.numeric(data$HR)
+    }
+  } else {
+    stop('User-defined HR name must be character.')
   }
 
-    {
-  #   }else{
-  #
-  #     if(hr > ncol(data)){
-  #
-  #       stop('Invalid index for HR. Index greater than number of available columns')
-  #     }
-  #
-  #     if(hr < 0){
-  #
-  #       stop('Invalid index for HR. Cannot have negative index')
-  #     }
-  #
-  #
-  #     # Check this, not entirely sure if this logic is correct... will this work if multiple changes? tested with a few examples, but
-  #     # not sure if my tests were thorough enough. I feel like I'm missing something here
-  #     if(sbp != 1 | dbp != 2 | is.null(pp) | if(!is.null(pp)){pp != 3}){
-  #       hr <- hr + 1
-  #     }
-  #     # same as above... check for more tests
-  #     if(is.null(pp)){
-  #       hr_inc <- 4
-  #     }else{
-  #       hr_inc <- 3
-  #     }
-  #
-  #     data <- data[, c(1:3, hr, (4:ncol(data))[-hr + hr_inc])]
-  #   }
-  # }
-}
+
 
 
 
@@ -329,41 +241,6 @@ process_data <- function(data, sbp = NULL, dbp = NULL, bp_datetime = NULL, id = 
 
   }
 
-    {
-  #   }else{
-  #
-  #     if(rpp > ncol(data)){
-  #
-  #       stop('Invalid index for RPP. Index greater than number of available columns')
-  #
-  #       }
-  #
-  #     if(rpp < 0){
-  #
-  #       stop('Invalid index for RPP. Cannot have negative index')
-  #
-  #       }
-  #
-  #     # This needs to be fixed:
-  #     if(rpp != ncol(data)){
-  #
-  #       rpp <- rpp + 2
-  #       data <- data[, c(1:4, rpp, (5:ncol(data))[-(rpp)])]
-  #
-  #     }else{
-  #
-  #       data <- data[, c(1:4, rpp)]
-  #
-  #     }
-  #   }
-
-  # # Add RPP column if none exist
-  # if(is.null(idx_list$rpp) & (!is.null(idx_list$sbp) & !is.null(idx_list$hr))){
-  #   data$RPP <- data[, idx_list$sbp] * data[, idx_list$hr]
-  #   idx_list$rpp <- ncol(data)
-  #   message("No RPP column found. Automatically added using SBP and HR.")
-  # }
-    }
 
 
 
@@ -378,7 +255,7 @@ process_data <- function(data, sbp = NULL, dbp = NULL, bp_datetime = NULL, id = 
     }
   } else if(toupper(map) %in% colnames(data) == FALSE){
 
-      stop('User-defined MAP name does not match column name of supplied dataset')
+    stop('User-defined MAP name does not match column name of supplied dataset')
 
   } else {
 
@@ -392,17 +269,27 @@ process_data <- function(data, sbp = NULL, dbp = NULL, bp_datetime = NULL, id = 
 
 
 
-  # Wake (1: Awake | 2: Asleep)
+  # Wake (1: Awake | 0: Asleep)
   if(!is.null(wake)){
 
     if(toupper(wake) %in% colnames(data) == FALSE){
 
       stop('User-defined ID name does not match column name of supplied dataset')
 
-    } else {
+    }
 
-      col_idx <- grep(paste("\\b",toupper(wake),"\\b", sep = ""), names(data))
-      colnames(data)[col_idx] <- "WAKE"
+    col_idx <- grep(paste("\\b",toupper(wake),"\\b", sep = ""), names(data))
+    colnames(data)[col_idx] <- "WAKE"
+
+    if(length(unique(data$WAKE)) > 2){
+
+      warning('Ignoring wake argument. Wake column must only contain 2 unique values corresponding to awake or asleep status. Typically, these are denoted as 1 for Awake and 0 for Asleep.')
+
+    }else{
+
+      data$WAKE <- as.integer(data$WAKE) # coerce to integers
+
+      # Assuming there are only two unique values, move column to beginning of df
       data <- data[, c(col_idx, (1:ncol(data))[-col_idx])]
 
     }
@@ -424,6 +311,8 @@ process_data <- function(data, sbp = NULL, dbp = NULL, bp_datetime = NULL, id = 
 
       col_idx <- grep(paste("\\b",toupper(visit),"\\b", sep = ""), names(data))
       colnames(data)[col_idx] <- "VISIT"
+
+      data$VISIT <- as.integer(data$VISIT)
       data <- data[, c(col_idx, (1:ncol(data))[-col_idx])]
 
     }
@@ -436,7 +325,7 @@ process_data <- function(data, sbp = NULL, dbp = NULL, bp_datetime = NULL, id = 
   # Date & Time (DateTime object)
   if(!is.null(bp_datetime)){
 
-      if(toupper(bp_datetime) %in% colnames(data) == FALSE){
+    if(toupper(bp_datetime) %in% colnames(data) == FALSE){
 
       stop('User-defined bp_datetime name does not match column name of supplied dataset')
 
@@ -446,7 +335,7 @@ process_data <- function(data, sbp = NULL, dbp = NULL, bp_datetime = NULL, id = 
       colnames(data)[col_idx] <- "DATE_TIME"
       data <- data[, c(col_idx, (1:ncol(data))[-col_idx])]
 
-      data$DATE_TIME <- as.POSIXct(data$DATE_TIME)
+      data$DATE_TIME <- as.POSIXct(data$DATE_TIME) # coerce to proper time format
     }
   }
 
