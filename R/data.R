459b8c36d381b4703e08d39909eea0cdd7b8c69e
John Schwenck <jschwenck@stat.tamu.edu>
2021-01-27 22:28:25 -0600

    fixed inconsistencies in data (dates different than datetime values, etc)

--- R/data.R
+++ R/data.R
@@ -11,7 +11,6 @@
 #' \describe{
 #'
 #' \item{DateTime}{A POSIXct-formatted column corresponding to the date and time of the corresponding reading in local time.}
-#' \item{Date}{A Date-formatted column indicating the date of the corresponding reading up until 4AM the following day.}
 #' \item{Month}{Integer. The month corresponding to the Date column.}
 #' \item{Day}{Integer. The day of the month corresponding to the Date column.}
 #' \item{Year}{Integer. The year corresponding to the Date column.}
@@ -20,9 +19,7 @@
 #' \item{Hour}{Integer. The hour corresponding to the DateTime column.}
 #' \item{Meal_Time}{Character. The estimated meal time corresponding to the DateTime column.}
 #' \item{Sys.mmHg.}{Integer. The systolic blood pressure reading.}
-#' \item{SysExcess}{Integer. The difference between the Sys.mmHg. column and the pre-determined 135 systolic blood pressure threshold.}
 #' \item{Dias.mmHg.}{Integer. The diastolic blood pressure reading.}
-#' \item{DiaExcess}{Integer. The difference between the Dias.mmHg. column and the pre-determined 85 diastolic blood pressure threshold.}
 #' \item{bpDelta}{Integer. The difference between the Sys.mmHg. and Dias.mmHg. column. This is also known as the Pulse Pressure.}
 #' \item{Pulse.bpm.}{Integer. The heart rate value (measured in beats per minute -- bpm).}
 #'
@@ -48,13 +45,11 @@
 #' \item{HR}{Integer. The heart rate value (measured in beats per minute -- bpm).}
 #' \item{PP}{Integer. The pulse pressure value calculated as the systolic value - the diastolic value.}
 #' \item{RPP}{Integer. The rate pressure product calculated as the systolic reading multiplied by the heart rate value.}
-#' \item{COMMENT}{Character. Text pertaining to specific readings or individuals. This has been omitted for privacy reasons.}
 #' \item{WAKE}{Integer. A logical indicator value corresponding to whether or not a subject is awake (\code{WAKE = 1}) or not.}
 #' \item{ID}{Integer. A unique identifier for each subject.}
 #' \item{VISIT}{Integer. A value associated with the visit number or a particular subject.}
 #' \item{DATE}{Character. A date-valued column indicating the date of the given reading. Dates are specified according to their
 #' actual date (i.e. 01:00 corresponds to the next date, even if the subject is awake. Awake-state is indicated via \code{WAKE}).}
-#' \item{TIME}{Character. A time value corresponding to the DATE.TIME column.}
 #'
 #' }
 #'
